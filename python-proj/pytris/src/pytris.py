7a80c97a426d0b9c38ed09d557439e0bc5364d60
messiahsb <bonet.messiah@gmail.com>
2026-03-22 00:15:43 -0400

    added some tetris code

--- python-proj/pytris/src/pytris.py
+++ python-proj/pytris/src/pytris.py
@@ -2,9 +2,48 @@
 import pygame
 import random
 
+#display dimensions
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
+#game dimensions
+BLOCK_SIZE = 30 
+GRID_WIDTH = 10 
+GRID_HEIGHT = 20 
+BORDER_WIDTH = 4
+
+#colors
 BLACK  = (0,0,0)
+WHITE = (255, 255, 255) 
+CYAN = (0, 255, 255)
+YELLOW = (255, 255, 0)
+MAGENTA = (255, 0, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+ORANGE = (255, 165, 0)
+GRAY = (128, 128, 128) 
+
+SHAPES = [
+    [1, 1, 1, 1],  #I Shape
+
+    [[1, 1], #O shape
+     [1, 1]],
+
+    [[0,1,0] #T shape
+     [1,1,1]],
+
+    [[1,1,0] #Z shape
+     [0,1,1]],
+
+    [[0,1,1] #S shape
+     [1,1,0]],
+
+    [[1,0,0] #J shape
+     [1,1,1]],
+
+    [[0,0,1] #L shape
+     [1,1,1]],
+]
 #initialize pygame
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -26,7 +65,8 @@ while running:
     
 pygame.quit()
 
-
-
 class Piece(object):
-    pass    
+    pass
+
+class Tetris():
+    pass
