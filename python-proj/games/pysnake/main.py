5248a33b3a6db648051711c274d476737b4b16bf
messiahsb <bonet.messiah@gmail.com>
2026-03-30 23:19:53 -0400

    added eating fucntionality and snake movement

--- python-proj/games/pysnake/main.py
+++ python-proj/games/pysnake/main.py
@@ -20,26 +20,85 @@ GREEN = (173, 204, 96)
 DARK_GREEN = (43, 52, 24)
 
 class Food():
-    def __init__(self):
-        self.position = Vector2(random.randint(0, 30),random.randint(0, 25))
-    
+    def __init__(self, snake):
+        self.position = self.gen_new_pos(snake)
+
     def draw(self):
         food_rect = pygame.Rect(self.position.x*CELL_SIZE, self.position.y*CELL_SIZE, CELL_SIZE, CELL_SIZE)
-        pygame.draw.rect(screen, DARK_GREEN, food_rect)
+        pygame.draw.ellipse(screen, 'red', food_rect)
 
+    def gen_new_pos(self, snake):
+        pos = Vector2(random.randint(0, NUMBER_CELLS-1), random.randint(0, NUMBER_CELLS-1)) 
+        while pos in snake:
+            pos = Vector2(random.randint(0, NUMBER_CELLS-1), random.randint(0, NUMBER_CELLS-1)) 
+        return pos
 
+class Snake():
+    def __init__ (self):
+        self.body = [Vector2(0, 0)]
+        self.direction = Vector2(0,0)
+        
+    def draw(self):
+        for seg in self.body:
+            seg_rect = (seg.x * CELL_SIZE, seg.y * CELL_SIZE, CELL_SIZE, CELL_SIZE)
+            pygame.draw.rect(screen, DARK_GREEN, seg_rect, 0, 7)
+   
+    def move_snake(self, dir):
+        self.direction = dir
+    
+    def update(self):
+        if len(self.body) > 1:
+            self.body = self.body[:-1]
+            self.body.insert(0, self.body[0] + self.direction)
+        else:
+            self.body[0]+=self.direction
 
-food = Food()
+class Game():
+    def __init__(self):
+        self.snake = Snake()        
+        self.food = Food(self.snake.body)
+
+    def draw(self):
+        self.food.draw()
+        self.snake.draw()
+
+    def update(self):
+        self.snake.update()
+        self.check_eat()
+
+    def check_eat(self):
+        if self.snake.body[0] == self.food.position:
+            self.food.position = self.food.gen_new_pos(self.snake.body)
+        
+    
+SNAKE_UPDATE = pygame.USEREVENT
+pygame.time.set_timer(SNAKE_UPDATE, 200)
+
 running = True
+game = Game()
+snake = game.snake
+food = game.food
 
 while running:
     for event in pygame.event.get():
+        if event.type == SNAKE_UPDATE: 
+                game.update()
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP and snake.direction!= Vector2(0,1):
+                snake.move_snake(Vector2(0,-1))
+            elif event.key == pygame.K_DOWN and snake.direction!= Vector2(0,-1):
+                snake.move_snake(Vector2(0,1))
+            elif event.key == pygame.K_RIGHT and snake.direction != Vector2(-1,0):
+                snake.move_snake(Vector2(1,0))
+            elif event.key == pygame.K_LEFT and snake.direction != Vector2(1,0):
+                snake.move_snake(Vector2(-1,0))
 
+    
     screen.fill(GREEN)
-    food.draw()
+    game.draw()
 
     pygame.display.update()
     clock.tick(60)
